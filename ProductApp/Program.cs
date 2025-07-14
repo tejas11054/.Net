d6fc1e841c177ada1c1c6d775c454ccf99d6f5a9
Tejas Ramane <140798292+tejas11054@users.noreply.github.com>
2025-07-14 09:45:38 +0530

    Add files via upload

---
+++ ProductApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product(101, "Laptop", 60000, 10);
+            Product p2 = new Product(102, "Smartphone", 35000, 15);
+
+            p1.DisplayDetails();
+            p2.DisplayDetails();
+        }
+    }
+}
