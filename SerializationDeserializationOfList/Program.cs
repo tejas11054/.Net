b65f53546384c284d09438ffc94a13e4b06f29c1
Tejas Ramane <140798292+tejas11054@users.noreply.github.com>
2025-07-14 09:18:49 +0530

    Add files via upload

---
+++ SerializationDeserializationOfList/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+
+namespace Serialization_Deserialization
+{
+    internal class Program
+    {
+        static string filePath = $"D:\\Swabhav\\Assignment\\persons.json";
+        static void Main(string[] args)
+        {
+             if(File.Exists(filePath))
+            {
+                //Deserialize
+                string json = File.ReadAllText(filePath) ;
+                List<Person> person = JsonSerializer.Deserialize<List<Person>>(json) ;
+
+                Console.WriteLine("Deserialized person: ");
+                foreach(var p in person)
+                {
+                    Console.WriteLine($"ID: {p.Id}, Name: {p.Name}, Email: {p.Email}");
+                }
+                Console.WriteLine($"Total Persons: {person.Count}");
+            }
+
+            else
+            {
+                //Serialize
+                List<Person> person = new List<Person>
+                {
+                    new Person { Id = 1, Name= "ABC", Email = "abc@gmail.com" },
+                    new Person { Id = 2, Name= "XYZ", Email = "xyz@gmail.com" },
+                    new Person { Id = 3, Name= "PQR", Email = "pqr@gmail.com" },
+                    new Person { Id = 4, Name= "DEF", Email = "def@gmail.com" },
+                    new Person { Id = 5, Name= "STQ", Email = "stq@gmail.com" }
+                };
+
+                string json = JsonSerializer.Serialize(person, new JsonSerializerOptions { WriteIndented = true }); 
+                File.WriteAllText(filePath, json);
+                Console.WriteLine("5 Person objects serialized to person.json");
+            }
+
+
+
+        }
+    }
+}
