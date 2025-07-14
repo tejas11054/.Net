9223d6d3d801c3ac96db7017dc15f5641c63f2e4
Tejas Ramane <140798292+tejas11054@users.noreply.github.com>
2025-07-14 09:57:14 +0530

    Add files via upload

---
+++ StudentGrade/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("ABC");
+            student1.Marks = 92;
+            student1.AssignGrade();
+            student1.DisplayInfo();
+
+            Student student2 = new Student("XYZ", 85);
+            student2.AssignGrade();
+            student2.DisplayInfo();
+
+            Student student3 = new Student("PQR", 65, 'D');
+            student3.AssignGrade();
+            student3.DisplayInfo();
+        }
+    }
+}
---
+++ StudentGrade/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Marks { get; set; }
+        public char Grade { get; set; }
+
+        public Student(string name)
+        {
+            Name = name;
+            Marks = 0;
+            Grade = 'D';
+        }
+
+        public Student(string name, int marks)
+        {
+            Name = name;
+            Marks = marks;
+            Grade = 'D';
+        }
+
+        public Student(string name, int marks, char grade)
+        {
+            Name = name;
+            Marks = marks;
+            Grade = grade;
+        }
+
+        public void AssignGrade()
+        {
+            if (Marks >= 90)
+                Grade = 'A';
+            else if (Marks >= 80)
+                Grade = 'B';
+            else if (Marks >= 70)
+                Grade = 'C';
+            else
+                Grade = 'D';
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Marks: {Marks}, Grade: {Grade}");
+        }
+    }
+}
