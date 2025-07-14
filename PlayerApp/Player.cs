d769f0a7fce3a083bf154bbe5e29e5f4e3de6c73
Tejas Ramane <140798292+tejas11054@users.noreply.github.com>
2025-07-14 09:29:48 +0530

    Add files via upload

---
+++ PlayerApp/Player.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp
+{
+    public class Player
+    {
+        public readonly int id;
+        public readonly string name;
+        public readonly int age;
+
+        public Player(int id, string name, int age)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+
+        public Player(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+            age = 21;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public int getAge()
+        {
+            return age;
+        }
+
+        public Player WhoIsElder(Player player)
+        {
+            if (player.age > this.age)
+                return player;
+            else
+                return this;
+        }
+    }
+}
---
+++ PlayerApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PlayerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player(101, "Tejas", 22);
+            Player player2 = new Player(102, "Rushabh");
+
+            Player result = player2.WhoIsElder(player1);
+            Console.WriteLine("\nDetails of older player: ");
+            Console.WriteLine($"ID = {result.getId()}, Age = {result.getAge()}, Name = {result.getName()}");
+
+        }
+    }
+}
