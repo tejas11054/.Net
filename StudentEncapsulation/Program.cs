38b4fd61671d832a001339943015c431475db5c0
Tejas Ramane <140798292+tejas11054@users.noreply.github.com>
2025-07-14 09:46:50 +0530

    Add files via upload

---
+++ StudentEncapsulation/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace StudentEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+
+            Console.WriteLine("Enter Roll number: ");
+            int roll = int.Parse(Console.ReadLine());
+            s.RollNo = roll;
+
+            Console.WriteLine("Enter Name: ");
+            string name = Console.ReadLine();
+            s.Name = name;
+
+            Console.WriteLine("Enter CGPA: ");
+            double cgpa = double.Parse(Console.ReadLine());
+            s.Gpa = cgpa;
+
+            Console.WriteLine("\nStudent Details- ");
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ StudentEncapsulation/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEncapsulation
+{
+    internal class Student
+    {
+        private int rollNo;
+        private string name;
+        private double cgpa;
+
+        public int RollNo
+        {
+            get { return rollNo; }
+            set
+            {
+                if (value > 0)
+                    rollNo = value;
+                else
+                    rollNo = 0;
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length > 5)
+                    name = value;
+                else
+                    name = "";
+            }
+        }
+        
+        public double Gpa
+        {
+            get { return cgpa; }
+            set
+            {
+                if(value >=1 && value <= 10)
+                    cgpa = value;
+                else
+                    cgpa = 0;
+            }
+        }
+
+        public double GetPercentage()
+        {
+            return cgpa * 9.5;
+        }
+
+        public override string ToString()
+        {
+            return $"Roll No           : {RollNo}\n" +
+                   $"Name:             : {(string.IsNullOrEmpty(Name) ? "Invalid Name" : Name)}\n" +
+                   $"CGPA              : {cgpa}\n" +
+                   $"Percentage        : {GetPercentage()}";
+        }
+    }
+}
