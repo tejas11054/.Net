d89a50b8597cdf6466e769ca73ecf7a51c56768a
Tejas Ramane <140798292+tejas11054@users.noreply.github.com>
2025-07-14 09:53:41 +0530

    Add files via upload

---
+++ BankManagement/Account.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BankManagement
+{
+    internal class Account
+    {
+        private static Random rand = new Random();
+        private static string[] existingAccountNumbers = new string[3];
+        private static int accountCount = 0;
+
+        public string AccountNumber { get; set; }
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string AccountType { get; set; }
+
+        public Account(string name, string accounttype, double initialDeposit)
+        {
+            Name = name;
+            AccountType = accounttype;
+            Balance = initialDeposit >= 500 ? initialDeposit : 500;
+            AccountNumber = GenerateUniqueAccountNumber();
+        }
+
+        public string GenerateUniqueAccountNumber()
+        {
+            string accountNumber;
+            do
+            {
+                accountNumber = "IDBI1000" + rand.Next(1000000, 9999999);
+            } while(Array.Exists(existingAccountNumbers, num => num ==  accountNumber));
+
+            existingAccountNumbers[accountCount++] = accountNumber;
+            return accountNumber;
+        }
+
+        public void Deposit(double amount)
+        {
+            if(amount > 0)
+            {
+                Balance += amount;
+                Console.WriteLine("Amount Deposited Successfully");
+            }
+            else
+            {
+                Console.WriteLine("Invalid deposit amount");
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && Balance - amount >= 500)
+            {
+                Balance -= amount;
+                Console.WriteLine("Amount withdrawn Successfully");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance");
+            }
+        }
+
+        public override string ToString()
+        {
+            string value = $"Account Number: {AccountNumber}\t Name: {Name}\t Account Type: {AccountType}\t Balanec: {Balance}";
+            return value;
+        }
+    }
+}
---
+++ BankManagement/Program.cs
@@ -0,0 +1,119 @@
+﻿namespace BankManagement
+{
+    internal class Program
+    {
+        static Account[] accounts = new Account[3];
+        static int index = 0;
+        
+        static void ShowMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. Create an Account");
+                Console.WriteLine("2. View Balance");
+                Console.WriteLine("3. Deposit");
+                Console.WriteLine("4. Withdraw");
+                Console.WriteLine("5. Exit");
+                Console.Write("Enter your choice: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        CreateAccount();
+                        break;
+
+                    case 2:
+                        ViewBalance();
+                        break;
+
+                    case 3:
+                        Deposit();
+                        break;
+
+                    case 4:
+                        Withdraw();
+                        break;
+
+                    case 5:
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+            }
+        }
+
+        static void CreateAccount()
+        {
+            if(index >=  accounts.Length)
+            {
+                Console.WriteLine("Account limit reached");
+                return;
+            }
+
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter account type (savings/current): ");
+            string type = Console.ReadLine();
+            Console.Write("Enter initial deposit amount: ");
+            double amount = double.Parse(Console.ReadLine());
+
+            accounts[index] = new Account(name, type, amount);
+            Console.WriteLine("Account has been created successfully");
+            Console.WriteLine(accounts[index].ToString());
+            index++;
+        }
+
+        static Account FindAccount()
+        {
+            Console.Write("Enter account number: ");
+            string accountNumber = Console.ReadLine();
+
+            for(int i=0; i<accounts.Length; i++)
+            {
+                if(accounts[i].AccountNumber == accountNumber)
+                    return accounts[i];
+            }
+            Console.WriteLine("Account not found");
+            return null;
+        }
+
+        static void ViewBalance()
+        {
+            var account = FindAccount();
+            if(account != null) 
+                Console.WriteLine($"Balance: {account.Balance}");
+        }
+
+        static void Deposit()
+        {
+            var account = FindAccount();
+            if (account != null)
+            {
+                Console.Write("Enter amount to deposit: ");
+                double amount = double.Parse(Console.ReadLine());
+                account.Deposit(amount);
+            }
+        }
+
+        static void Withdraw()
+        {
+            var account = FindAccount();
+            if (account != null)
+            {
+                Console.Write("Enter amount to withdraw: ");
+                double amount = double.Parse(Console.ReadLine());
+                account.Withdraw(amount);
+            }
+                
+        }
+        static void Main(string[] args)
+        {
+            ShowMenu();
+
+        }
+    }
+}
