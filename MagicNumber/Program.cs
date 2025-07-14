72d890a263e82ae98965e51d05f40c2d32e9fd6b
Tejas Ramane <140798292+tejas11054@users.noreply.github.com>
2025-07-14 09:43:14 +0530

    Add files via upload

---
+++ MagicNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace MagicNumber
+{
+    internal class Program
+    {
+        static int SumOfDigits(int num)
+        {
+            while(num >= 10)
+            {
+                int sum = 0;
+                while (num > 0)
+                {
+                    sum += num % 10;
+                    num /= 10;
+                }
+                num = sum;
+            }
+            return num;
+        }
+
+        static bool IsMagicNumber(int num)
+        {
+            return SumOfDigits(num) == 1;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter any Number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (IsMagicNumber(num))
+                Console.WriteLine($"{num} is a magic number");
+            else
+                Console.WriteLine($"{num} is not a magic number");
+        }
+    }
+}
