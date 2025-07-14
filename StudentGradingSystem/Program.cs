42a4f9a69c98d20bafd49edaf22c44c2a4457c8c
Tejas Ramane <140798292+tejas11054@users.noreply.github.com>
2025-07-14 09:48:00 +0530

    Add files via upload

---
+++ StudentGradingSystem/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace StudentGradingSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter student name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter roll number");
+            string rollnumber = Console.ReadLine();
+
+            int[] marks = new int[5];
+            for(int i = 0; i < marks.Length; i++)
+            {
+                Console.Write("Enter marks for subject " + (i + 1));
+                marks[i] = int.Parse(Console.ReadLine());
+            }
+
+            Student student = new Student(name, rollnumber, marks);
+            Console.WriteLine(student);
+        }
+    }
+}
---
+++ StudentGradingSystem/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentGradingSystem
+{
+    internal class Student
+    {
+        private string studentName;
+        private string rollNumber;
+        private int[] marks;
+
+        public Student(string studentName, string rollNumber, int[] marks)
+        {
+            this.studentName = studentName;
+            this.rollNumber = rollNumber;
+            this.marks = marks;
+        }
+
+        public double CalculateAverage()
+        {
+            return marks.Average();
+        }
+
+        public string CalculateGrade()
+        {
+            double average = marks.Average();
+
+            if (average >= 90) return "A+";
+            else if (average >= 80) return "A";
+            else if (average >= 70) return "B";
+            else if (average >= 60) return "C";
+            else if (average >= 50) return "D";
+            else return "Fail";
+        }
+
+        public override string ToString()
+        {
+            string markList = "[" + string.Join(", ", marks) + "]";
+            double average = CalculateAverage();
+            string grade = CalculateGrade();
+
+            return $"Student Report Card\n" +
+                $"-------------------------\n" +
+                $"Name      : {studentName}\n" +
+                $"Roll No   : {rollNumber}\n" +
+                $"Marks     : {markList}\n" +
+                $"Average   : {average}\n" +
+                $"Grade     : {grade}\n";
+        }
+    }
+}
